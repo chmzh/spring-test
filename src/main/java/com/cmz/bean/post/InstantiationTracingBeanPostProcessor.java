ca9e0fba7730e1c35ff5e5e5a059020bf3ed65e6
chenmingzhou <sd@dfh.com>
2016-08-11 16:15:55 +0800

    TestController

--- src/main/java/com/cmz/bean/post/InstantiationTracingBeanPostProcessor.java
+++ src/main/java/com/cmz/bean/post/InstantiationTracingBeanPostProcessor.java
@@ -4,12 +4,16 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class InstantiationTracingBeanPostProcessor implements BeanPostProcessor {
-
+	/**
+	 * 该方法在初始化之前调用 在 @PostConstruct 方法之前
+	 */
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		System.out.println("Bean '" + beanName + "' created before : " + bean.toString());
 		return bean;
 	}
-
+	/**
+	 * 该方法在初始化之后调用 在 @PostConstruct 方法之后
+	 */
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 		System.out.println("Bean '" + beanName + "' created : " + bean.toString());
 		return bean;
